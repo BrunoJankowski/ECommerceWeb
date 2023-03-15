74131ff7779131053e433e5466c1ed85d20171a4
Bruno Jankowski <112755911+BrunoJankowski@users.noreply.github.com>
2023-03-15 11:42:48 +0100

    quantity added

--- site/src/main.js
+++ site/src/main.js
@@ -1,9 +1,12 @@
-var itemButton = document.getElementsByClassName("btn")
+var itemButtonRemove = document.getElementsByClassName("btn")
+var itemButtonSubmit = document.getElementsByClassName("submit")
 var priceItem = document.getElementsByClassName('price')
 var quantityItem = document.getElementsByClassName('quantity')
+
+var total = 0
 var prices = []
 var quantities = []
-var total = 0
+
 for (var i = 0; i < priceItem.length; i++) {
     prices.push(priceItem[i].innerHTML)
     quantities.push(quantityItem[i].value)
@@ -16,21 +19,23 @@ console.log(total);
 
 document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2); //sets a p to total
 
-for(var i = 0; i < itemButton.length; i++){
-    var button = itemButton[i]
-    button.setAttribute("data-index", i)
-    button.addEventListener("click", function(event){
+for(var i = 0; i < itemButtonRemove.length; i++){
+    var button_remove = itemButtonRemove[i]
+    button_remove.setAttribute("data-index", i)
+    button_remove.addEventListener("click", function(event){
         var buttonClicked = event.target
         var index = buttonClicked.getAttribute("data-index") // retrieve index from data attribute
         console.log(index);
-        updateTotal(index, prices)
+        updateTotalRemove(index, prices)
         buttonClicked.parentElement.parentElement.parentElement.remove()
     })
 }
 
-function updateTotal (index, prices){
+function updateTotalRemove (index, prices){
     var price = prices[index]
-    total -= parseFloat(price)
+    var quantity = quantities[index]
+    total -= parseFloat(price * quantity)
+
     document.getElementById("total").innerHTML = "Total: $ " + total.toFixed(2) // updates total to 2nd number after . 
     console.log(price);
     console.log(total);
