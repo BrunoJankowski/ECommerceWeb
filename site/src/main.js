8df277e9e2b83bd8bacdc68dc14e652ee837e7ec
Bruno Jankowski <112755911+BrunoJankowski@users.noreply.github.com>
2023-03-15 14:54:59 +0100

    Added cart and shop pages
    I fixed all the bugs with cart system, implemented a shop syste where you can add the items to cart

--- site/src/main.js
+++ site/src/main.js
@@ -1,5 +1,7 @@
 var itemButtonRemove = document.getElementsByClassName("btn")
-var itemButtonSubmit = document.getElementsByClassName("submit")
+var itemButtonPlus = document.getElementsByClassName("plus")
+var itemButtonMinus = document.getElementsByClassName("minus")
+
 var priceItem = document.getElementsByClassName('price')
 var quantityItem = document.getElementsByClassName('quantity')
 
@@ -13,29 +15,34 @@ for (var i = 0; i < priceItem.length; i++) {
     total += parseFloat(prices[i] * quantities[i])
   }
 
-console.log(quantities);
-console.log(prices);
-console.log(total);
-
 document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2); //sets a p to total
 
 for(var i = 0; i < itemButtonRemove.length; i++){
     var button_remove = itemButtonRemove[i]
-    var button_submit = itemButtonSubmit[i]
+    var button_plus = itemButtonPlus[i]
+    var button_minus = itemButtonMinus[i]
     button_remove.setAttribute("data-index", i)
-    button_submit.setAttribute("data-index", i)
+    button_plus.setAttribute("data-index", i)
+    button_minus.setAttribute("data-index", i)
+
     button_remove.addEventListener("click", function(event){
         var buttonClicked = event.target
         var index = buttonClicked.getAttribute("data-index") // retrieve index from data attribute
-        console.log(index);
+        
         updateTotalRemove(index, prices, quantities)
         buttonClicked.parentElement.parentElement.parentElement.remove()
     })
-    button_submit.addEventListener("click", function(event){
-        var buttonClickedSub = event.target
-        var index = buttonClickedSub.getAttribute("data-index") // retrieve index from data attribute
+    button_plus.addEventListener("click", function(event){
+        var plus = event.target
+        var index = plus.getAttribute("data-index") // retrieve index from data attribute
         
-        updateTotalAdd(index, prices)
+        updateTotalPlus(index, prices)
+    })
+    button_minus.addEventListener("click", function(event){
+        var minus = event.target
+        var index = minus.getAttribute("data-index") // retrieve index from data attribute
+        
+        updateTotalMinus(index, prices, minus)
     })
 }
 
@@ -44,13 +51,13 @@ function updateTotalRemove (index, prices){
     var quantity = quantities[index]
     total -= parseFloat(price * quantity)
 
-    document.getElementById("total").innerHTML = "Total: $ " + total.toFixed(2) // updates total to 2nd number after . 
+    document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2) // updates total to 2nd number after . 
     console.log(price);
     console.log(total);
 
 }
 
-function updateTotalAdd (index, prices){
+function updateTotalPlus (index, prices){
     var price = prices[index]
     total += parseFloat(price)
 
@@ -58,9 +65,25 @@ function updateTotalAdd (index, prices){
     quantities_value++;
     quantities[index]++;
     quantityItem[index].value = quantities_value
-    document.getElementById("total").innerHTML = "Total: $ " + total.toFixed(2) // updates total to 2nd number after . 
-    console.log(price);
-    console.log(total);
+    document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2) // updates total to 2nd number after . 
 
 }
 
+
+function updateTotalMinus (index, prices, buttonClicked){
+    var price = prices[index]
+
+    if(quantityItem[index].value > 0){
+        total -= parseFloat(price)
+        var quantities_value = quantities[index]
+        quantities_value--;
+        quantities[index]--;
+        quantityItem[index].value = quantities_value
+        document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2) // updates total to 2nd number after . 
+
+    }
+    else{
+        console.log("ITS 0");
+    }
+
+}
